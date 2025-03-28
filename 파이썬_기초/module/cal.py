ad9bbd657e3a368c285158c4845c3dd4df910125
HSClover <kkej1009@naver.com>
2025-03-28 15:23:42 +0900

    3/28 PM 1st commit

---
+++ 파이썬_기초/module/cal.py
@@ -0,0 +1,18 @@
+# 변수 정의
+PI = 3.141592
+
+# 사칙연산 함수 정의
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "0으로 나눌 수 없습니다."
