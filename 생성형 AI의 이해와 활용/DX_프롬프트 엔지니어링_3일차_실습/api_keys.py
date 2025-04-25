3b4d78d4053f69d40000625f20d216dbba6b442e
HSClover <kkej1009@naver.com>
2025-04-25 04:49:23 +0000

    3일차 자료

---
+++ 생성형 AI의 이해와 활용/DX_프롬프트 엔지니어링_3일차_실습/api_keys.py
@@ -0,0 +1,3 @@
+# api_keys.py 파일을 만들고, 그 안에 OPENAI_API_KEY 변수에 자신의 openai api key를 입력함
+
+OPENAI_API_KEY = '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'
