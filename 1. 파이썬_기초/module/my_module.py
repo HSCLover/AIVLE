becdbfc5a3cd2e05ad8d87d94aeb16d11e167a94
HSClover <kkej1009@naver.com>
2025-05-07 15:34:40 +0900

    파일 위치 및 이름 정리리

---
+++ 1. 파이썬_기초/module/my_module.py
@@ -0,0 +1,8 @@
+import math
+
+def radius_area(radius: float) -> float:
+    """반지름을 입력받아 원의 넓이를 계산
+
+    `Args` radius (_float_): 반지름, `Returns` _int_: 원의 넓이
+    """
+    return math.pi * radius ** 2
